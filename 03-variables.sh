b3c258e81e81d3cb27707b167ab12f491c0dc5e0
viswa150160 <viswa160150@gmail.com>
2024-04-02 15:12:23 +0530

    correct the mistek

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "hellow world"
+
+person1=$1  # This is first command line
+person2=$2  # This is 2nd command line
+Person3=$3  # this is 3rd command line
+
+echo "$person1: Hi $person2"
+echo "$person2: Hi $person1 good morning"
+echo "$person1 : Good moring "
